316c118bd765c251a45039dd871c6dd085bdc2ff
Vitalina28 <132218868+Vitalina28@users.noreply.github.com>
2023-10-04 22:40:39 +0200

    corrected js

--- js/task-10.js
+++ js/task-10.js
@@ -1,10 +1,29 @@
 const inputEl = document.querySelector('#controls');
 const createBtn = document.querySelector('[data-create]');
-const destroyBtn = document.querySelector('[data-destroy]')
-console.log(inputEl);
-console.log(createBtn);
-console.log(destroyBtn);
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
 
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = inputEl.value;
+  const boxes = [];
+
+  for (let i = 0; i > amount; i += 1) {
+    const box = document.createElement('div');
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+
+  boxesEl.append(...boxes);
+} 
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
