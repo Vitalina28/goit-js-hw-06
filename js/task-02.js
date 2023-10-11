7ae746f96de27d1dbd8e326ea247737a6f7ef5c1
Vitalina28 <132218868+Vitalina28@users.noreply.github.com>
2023-10-11 16:25:32 +0200

    corected js2

--- js/task-02.js
+++ js/task-02.js
@@ -8,15 +8,15 @@ const ingredients = [
 ];
 
 
-const listEl = document.querySelector('#ingredients');
+const listEl = document.querySelector('#ingredients');  
 
-  
-
-  ingredients.forEach(element => {
+const elements = ingredients.map(element => {
   const itemEl = document.createElement('li');
   itemEl.textContent = element;
   itemEl.classList.add("item");
-    console.log(itemEl);
     
-    listEl.appendChild(itemEl);
+  return itemEl;
 });
+console.log(elements);
+
+listEl.append(...elements);
